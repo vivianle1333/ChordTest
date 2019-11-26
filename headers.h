3a933d70df59ecc3c2e2928ffa46de67bb149b72
vivianle1333 <vivian.le1333@hotmail.com>
2019-11-26 17:18:37 -0500

    vivian mobile

---
+++ headers.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+//added files
+//#include <io.h>
+#include <unistd.h>
+#include <vector>
+#include <cstdlib>
+#include <openssl/sha.h>
+//#include <libcurl4-openssl-dev/openssl/sha.h>
+#include <sys/types.h>
+#include <thread>
+#include <ctime>
+//added files
+// #include <winsock2.h>
+// #include <ws2tcpip.h>
+//#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cmath>
+#include <mutex>
+
+using namespace std;
